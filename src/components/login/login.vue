33c45367bde9a57ba8966740070c156d6c250b8e
apple <apple@appledeMacBook-Pro-2.local>
2020-02-20 22:53:40 +0800

    提示框

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,9 +1,104 @@
 <template>
-  <div>login</div>
+  <div class="login-wrap">
+    <h2>用户登录</h2>
+    <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
+      <el-input type="text" placeholder="UserName" v-model="formdata.username"></el-input>
+      <el-input type="password" placeholder="PassWord" v-model="formdata.password"></el-input>
+      <el-button @click.prevent="handleLogin()" class="login-btn" type="primary">用户登录</el-button>
+      <el-button @click.prevent="anonymousLogin()" class="login-btn" type="primary">匿名登录</el-button>
+    </el-form>
+  </div>
 </template>
 
 <script>
+  export default {
+    data(){
+      return{
+        formdata:{
+          username:'',
+          password:''
+        }
+      }
+    },
+    methods:{
+      /*发送登录请求：用户登录*/
+      handleLogin(){
+        this.$http.post('login',this.formdata).then(res => {
+          const{
+            data,meta:{msg,status}
+          } = res.data
+          if(status === 200){
+            // this.$router.push({name:'home'})
+            this.$message.success(msg);
+          }else{
+            this.$message.warning(msg);
+          }
+        })
+      },
+      /*匿名登录*/
+      anonymousLogin(){
+        this.$message.success("登录成功！");
+        this.$router.push({name:'home'})
+      }
+    }
+  }
 </script>
 
 <style>
+  body{
+    background-color: #34495e;
+  }
+  .login-wrap{
+    width: 300px;
+    padding: 40px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    background-color: #191919;
+    text-align: center;
+  }
+  .login-wrap h2{
+    color: white;
+    text-transform: uppercase;
+    font-weight: 500;
+  }
+  .login-wrap .login-form input[type="text"],
+  .login-wrap .login-form input[type="password"]
+  {
+    border: 0;
+    background: none;
+    display: block;
+    margin: 20px auto;
+    text-align: center;
+    border: 2px solid #3498db;
+    padding: 14px 10px;
+    width: 200px;
+    outline: none;
+    color: white;
+    border-radius: 24px;
+    transition: 0.25s;
+  }
+  .login-wrap .login-form input[type="text"]:focus,
+  .login-wrap .login-form input[type="password"]:focus{
+    width: 280px;
+    border-color: #2ecc71;
+  }
+  .login-wrap .login-btn{
+    border: 0;
+    background: none;
+    display: block;
+    margin: 20px auto;
+    text-align: center;
+    border: 2px solid #2ecc71;
+    padding: 14px 40px;
+    outline: none;
+    color: white;
+    border-radius: 24px;
+    transition: 0.25s;
+    cursor: pointer;
+  }
+  .login-btn:hover{
+    background: #2ecc71;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,16 @@ import Vue from 'vue'
 import App from './App'
 
 import ElementUI from 'element-ui'
+import MyServerHttp from '@/plugins/http.js'
+
 import 'element-ui/lib/theme-chalk/index.css'
+import '@/assets/css/reset.css'
 
 import router from './router'
 
 Vue.use(ElementUI)
+Vue.use(MyServerHttp)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
